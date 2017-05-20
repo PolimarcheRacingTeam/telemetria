c47f832799ac499e1287978533b4136d2b910fe1
michele9 <mike_55@hotmail.it>
2017-05-20 12:28:10 +0200

    Prova DMA
    Prova DMA

---
+++ daq/prova_adc_temp_dma/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'prova_adc_temp_dma' 
+ * Target:  'prova_adc_temp_dma' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
