f266796c14a1465990ca1eed3b1c239cd7b850d3
michele9 <mike_55@hotmail.it>
2017-06-04 11:26:29 +0200

    agg
    agg

---
+++ daq/prova-adc_timer driver/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'prova-adc_timer driver' 
+ * Target:  'prova-adc_timer driver' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
