24f07795338d3d8e33b4918df84fcab773a709be
sebgiles <mrsebgiles@gmail.com>
2017-07-13 20:31:28 +0200

    cambiato script di test ricezione

--- remote-dashboard/multicast.py
+++ remote-dashboard/multicast.py
@@ -4,8 +4,8 @@ s=socket(AF_INET, SOCK_DGRAM)
 s.bind(('',2017))
 i = 0;
 while True:
-  m=s.recvfrom(255)
-  #packet = struct.unpack('>20H3h',m)
-  print i
-  print m
+  m = s.recvfrom(255)[0]
+  packet = struct.unpack('>20H3h',m)
+  print packet
+  #print m
   i = i + 1
